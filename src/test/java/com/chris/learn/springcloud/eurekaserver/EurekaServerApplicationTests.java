d94518997a701198602e8a32da18794b37d32ddf
christin_chacko_qb <christin@qburst.com>
2023-12-05 00:13:05 +0530

    first commit

---
+++ src/test/java/com/chris/learn/springcloud/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.chris.learn.springcloud.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootTest
+@EnableEurekaServer
+class EurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
